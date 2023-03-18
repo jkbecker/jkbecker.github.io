7f03b062e00f0d3f3a26ec9c72461895318cf933
Johannes Becker <johannes@serval.sh>
2023-03-18 17:22:30 -0400

    testing tailwind

--- themes/kodama-theme/tailwind.config.js
+++ themes/kodama-theme/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html",  "./themes/"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      display: ['IBM Plex Mono', 'Menlo', 'monospace'],
+      body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+    },
+  },
   variants: {},
   plugins: [
       require('@tailwindcss/typography'),
