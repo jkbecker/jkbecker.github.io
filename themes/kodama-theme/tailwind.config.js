29527e9ffa696a59ea52a9efb1c5b295b2fd83fd
Johannes Becker <johannes@serval.sh>
2023-03-18 18:36:31 -0400

    typography test

--- themes/kodama-theme/tailwind.config.js
+++ themes/kodama-theme/tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html",  "./themes/"],
-  theme: {},
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#333',
+            a: {
+              color: '#134e4a',
+              '&:hover': {
+                color: '#0f766e',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   variants: {},
   plugins: [
       require('@tailwindcss/typography'),
