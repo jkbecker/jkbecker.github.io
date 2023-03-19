69048d5d65c5f2afd2e601969d61f5a9ffef75b7
Johannes Becker <johannes@serval.sh>
2023-03-18 20:31:52 -0400

    styling improvements for bibliography

--- themes/kodama-theme/tailwind.config.js
+++ themes/kodama-theme/tailwind.config.js
@@ -1,22 +1,6 @@
 module.exports = {
   content: ["./templates/**/*.html", "./theme/**/*.html",  "./themes/"],
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#333',
-            a: {
-              color: '#134e4a',
-              '&:hover': {
-                color: '#0f766e',
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  theme: {},
   variants: {},
   plugins: [
       require('@tailwindcss/typography'),
